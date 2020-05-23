ff1e46a08a6dfe64d2cfe3b020259e3353f2f143
melbarch <elbarchany@gmail.com>
2020-05-23 21:01:01 +0000

    Fix tailwind styling purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: ['./pages/**/*.tsx'],
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
