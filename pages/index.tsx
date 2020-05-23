2e3540e1afd9a61cb6d04c90c7286cb4ed71b2dd
melbarch <elbarchany@gmail.com>
2020-05-23 18:04:56 +0000

    init first layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 const HomePage = () => {
   return (
-    <div className="text-center text-gray-700 block">
-      <div className="font-extrabold text-4xl">
-        Hi there 🖐!
+    <div className="flex justify-center items-center flex-col">
+      <div className="my-3 p-3 lg:w-1/2 rounded-md shadow-md">
+        <a href="https://github.com/u2zv1wx/neut">
+          <h3 className="text-2xl font-bold">Show HN: A dependently-typed programming language with static memory management</h3>
+        </a>
+        <h5 className="text-gray-500">2 hours ago - 141 points</h5>
+        <h5 className="text-gray-500">by u2zv1wx </h5>
       </div>
+
+      <div className="my-3 p-3 lg:w-1/2 rounded-md shadow-md">
+        <a href="https://github.com/u2zv1wx/neut">
+          <h3 className="text-2xl font-bold">Show HN: A dependently-typed programming language with static memory management</h3>
+        </a>
+        <h5 className="text-gray-500">2 hours ago - 141 points</h5>
+        <h5 className="text-gray-500">by u2zv1wx </h5>
+      </div>
+
     </div>
   );
 }
