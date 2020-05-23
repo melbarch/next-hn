200103e26a2057f69f573f0ea68727fcdac88d67
melbarch <elbarchany@gmail.com>
2020-05-23 17:20:47 +0000

    Init repo

---
+++ pages/_app.tsx
@@ -0,0 +1,4 @@
+import App from 'next/app'
+import '../css/tailwind.css'
+
+export default App
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div className="text-center text-gray-700 block">
+      <div className="font-extrabold text-4xl">
+        Hi there 🖐!
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
