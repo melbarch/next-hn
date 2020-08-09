fcdfd2c17854aa8063f939d4c50cd6f8a50e1afa
melbarch <elbarchany@gmail.com>
2020-08-09 21:49:37 +0100

    Add pulse animation while loading data 🎞

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,23 @@ const fetcher = async (url: string) => {
 const HomePage = () => {
   const { data, error } = useSWR(API_URL, fetcher)
   if (error) return <div className="text-center text-red-700 font-bold text-3xl bg-gray-100">failed to load</div>
-  if (!data) return <div className="text-center text-gray-700 bg-gray-100">loading...</div>
+  if (!data) return (
+    <main className="flex justify-center items-center flex-col">
+      {Array.from(Array(10), (_, i) => i + 1).map((item: any) =>
+        <div key={item} className="m-2 p-3 w-4/5 lg:w-2/3 rounded-md shadow-md bg-white">
+          <div className="animate-pulse flex space-x-4">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+              <div className="space-y-2">
+                <div className="h-4 bg-gray-400 rounded"></div>
+                <div className="h-4 bg-gray-400 rounded w-5/6"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </main>
+  )
   return (
     <main className="flex justify-center items-center flex-col">
       {data.map((item: any) =>
